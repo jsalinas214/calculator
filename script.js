858e81d89981f4030657882b1f3f408e7b9de74a
James Salinas <jamessalinas@Jamess-Air.lan>
2022-09-13 12:33:32 -0400

    finished logic behind calculator (JS)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // display the current time.
 const hour = document.querySelector('.hour');
 const minute = document.querySelector('.minute');
+const value = document.querySelector('.value');
 
 // gray buttons on calculator.
 const acClear = document.querySelector('.ac');
@@ -49,3 +50,137 @@ const updateTime = () => {
 }
 setInterval(updateTime, 1000);
 updateTime();
+
+let valueStringInMemory = null;
+let operatorInMemory = null;
+
+
+// functionality for value
+const getValueAsString = () => value.textContent.split(',').join('');
+
+const getValueAsNumber = () =>  parseFloat(getValueAsString());
+
+const setStringAsValue = (valueString) => {
+   if (valueString[valueString.length - 1] === '.') {
+      value.textContent += '.';
+      return;
+   }
+
+   const [wholeNumString, decimalString] = valueString.split('.');
+   if (decimalString) {
+      value.textContent = parseFloat(wholeNumString).toLocaleString() + '.' + decimalString;
+   } else {
+      value.textContent = parseFloat(wholeNumString).toLocaleString();
+   }
+};
+
+const handleNumberClick = (numString) => {
+   const currentValueString = getValueAsString();
+   if (currentValueString === '0') {
+      setStringAsValue(numString);
+   } else {
+      setStringAsValue(currentValueString + numString);
+   }
+};
+
+const getOperationResultAsString = () => {
+   const currentValueNumber = getValueAsNumber();
+   const valueNumberInMemory = parseFloat(valueStringInMemory);
+   let newValueNumber;
+
+   if (operatorInMemory === 'addition') {
+      newValueNumber = valueNumberInMemory + currentValueNumber;
+   } else if (operatorInMemory === 'subtraction') {
+      newValueNumber = valueNumberInMemory - currentValueNumber;
+   } else if (operatorInMemory === 'multiplication') {
+      newValueNumber = valueNumberInMemory * currentValueNumber;
+   } else if (operatorInMemory === 'division') {
+      newValueNumber = valueNumberInMemory / currentValueNumber;
+   }
+
+   return newValueNumber.toString();
+};
+
+const handleOperatorClick = (operation) => {
+   const currentValueString = getValueAsString;
+
+   if (!valueStringInMemory) {
+      valueStringInMemory = currentValueString;
+      operatorInMemory = operation;
+      setStringAsValue('0');
+      return;
+   }
+
+   valueStringInMemory = getOperationResultAsString();
+   operatorInMemory = operation;
+   setStringAsValue('0');
+};
+
+
+// adding Event Listeners
+acClear.addEventListener('click', () => {
+   setStringAsValue('0');
+   valueStringInMemory = null;
+   operatorInMemory = null;
+});
+
+pmPlusMinus.addEventListener('click', () => {
+   const currentValueNumber = getValueAsNumber();
+   const currentValueString = getValueAsString();
+
+   if (currentValueString === '-0') {
+      setStringAsValue('0');
+      return
+   }
+
+   if (currentValueNumber >= 0) {
+      setStringAsValue('-' + currentValueString);
+   } else {
+      setStringAsValue(currentValueString.substring(1));
+   }
+});
+
+percentage.addEventListener('click', () => {
+   const currentValueNumber = getValueAsNumber();
+   const newValueNumber = currentValueNumber / 100;
+
+   setStringAsValue(newValueNumber.toString());
+   valueStringInMemory = null;
+   operatorInMemory = null;
+});
+
+
+addition.addEventListener('click', () => {
+   handleOperatorClick('addition');
+});
+subtraction.addEventListener('click', () => {
+   handleOperatorClick('subtraction');
+});
+multiplication.addEventListener('click', () => {
+   handleOperatorClick('multiplication');
+});
+division.addEventListener('click', () => {
+   handleOperatorClick('division');
+});
+
+equal.addEventListener('click', () => {
+   if (valueStringInMemory) {
+      setStringAsValue(getOperationResultAsString());
+      valueStringInMemory = null;
+      operatorInMemory = null;
+   }
+});
+
+for(let i = 0; i < numberArray.length; i++) {
+   const number = numberArray[i];
+   number.addEventListener('click', () => {
+      handleNumberClick(i.toString());
+   });
+}
+
+   decimal.addEventListener('click', () => {
+      const currentValueString = getValueAsString();
+      if (!currentValueString.includes('.')) {
+         setStringAsValue(currentValueString + '.');
+   }
+});
